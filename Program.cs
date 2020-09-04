6cd496a51d896502ae2f14e2cce7f34b43d48c7a
Sam Booth <ballersabooth@yahoo.com>
2020-09-04 10:39:13 -0500

    first add

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
